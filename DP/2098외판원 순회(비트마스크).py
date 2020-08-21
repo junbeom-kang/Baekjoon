af4a6fd59c90227a34769cefad4b00833db4fff4
Kjbjbjb <kgood10369@gmail.com>
2020-08-21 16:34:17 +0900

    no message

--- DP/2098외판원 순회(비트마스크).py
+++ DP/2098외판원 순회(비트마스크).py
@@ -7,16 +7,14 @@ adj=[list(map(int,input().split()))for _ in range(n)]
 dp=[[INF]*(1<<n)for _ in range(n)]
 def dfs(current,visit):
     if visit==(1<<n)-1:
-        if adj[current][0]:
-            return adj[current][0]
-        else:
+        if adj[current][0]==0:
             return INF
+        else:
+            return adj[current][0]
     if dp[current][visit]!=INF:
         return dp[current][visit]
     for i in range(1,n):
-        if visit&(1<<i)==0 and adj[current][i]!=0:
+        if not visit&(1<<i) and adj[current][i]!=0:
             dp[current][visit]=min(dp[current][visit],dfs(i,visit|(1<<i))+adj[current][i])
     return dp[current][visit]
-
-
 print(dfs(0,1))
