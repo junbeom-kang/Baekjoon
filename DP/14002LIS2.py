43a762b89b05856e948c161f9510d2a4b922be69
Kjbjbjb <kgood10369@gmail.com>
2020-08-29 03:59:37 +0900

    no message

--- DP/14002LIS2.py
+++ DP/14002LIS2.py
@@ -1,17 +1,6 @@
 import sys
+import bisect
 input=sys.stdin.readline
-def bs(v):
-    left=0
-    right=len(ans)-1
-    while left<=right:
-        mid=(left+right)//2
-        if v<=ans[mid]:
-            temp=mid
-            right=mid-1
-        elif v>ans[mid]:
-            left=mid+1
-    return temp
-
 n=int(input())
 stack=list(map(int,input().split()))
 ans=[stack[0]]
@@ -22,7 +11,7 @@ for i in stack:
         ans.append(i)
         reverse.append(len(ans)-1)
     else:
-        a=bs(i)
+        a=bisect.bisect_left(ans,i)
         ans[a]=i
         reverse.append(a)
     num += 1
