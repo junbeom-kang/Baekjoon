13dc4600022ddfcf9578c8ddddfb2d03ed4514d5
Kjbjbjb <kgood10369@gmail.com>
2020-08-31 01:53:57 +0900

    no message

--- DP/LCS2.py
+++ DP/LCS2.py
@@ -10,6 +10,19 @@ for i in range(1,ls+1):
         if first[j-1]==second[i-1]:
             dp[i][j]=dp[i-1][j-1]+1
         else:
-            print(dp[i-1][j],dp[i][j-1])
             dp[i][j]=max(dp[i-1][j],dp[i][j-1])
 print(dp[ls][lf])
+temp=dp[ls][lf]
+ans=[]
+x=ls+1
+y=lf+1
+for i in range(ls,-1,-1):
+    for j in range(lf,-1,-1):
+        if i>=x or j>=y:
+            continue
+        if dp[i][j]!=dp[i][j-1] and dp[i][j]!=dp[i-1][j] and dp[i][j]==temp:
+            ans.append(second[i-1])
+            x=i
+            y=j
+            temp-=1
+print(''.join(map(str,ans[::-1])))
---
+++ dic.py
@@ -0,0 +1,6 @@
+a=5
+b=4
+for i in range(a):
+    print(1)
+    a-=1
+print(a)
