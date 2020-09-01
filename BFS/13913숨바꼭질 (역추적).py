e58e795c9e1859a28aa6a2d016d3c382294253c5
Kjbjbjb <kgood10369@gmail.com>
2020-09-02 03:25:33 +0900

    no message

--- BFS/13913숨바꼭질 (역추적).py
+++ BFS/13913숨바꼭질 (역추적).py
@@ -11,51 +11,33 @@ def bfs(a,b):
             temp=Q.popleft()
             if temp+1<100001:
                 if check[temp+1]==-1:
-                    check[temp+1]=cnt
+                    check[temp+1]=temp
                     if temp+1==b:
-                        return check[b]
+                        return cnt
                     Q.append(temp+1)
             if temp-1>=0:
                 if check[temp-1]==-1:
-                    check[temp-1]=cnt
+                    check[temp-1]=temp
                     if temp-1==b:
-                        return check[b]
+                        return cnt
                     Q.append(temp-1)
             if 2*temp<100001:
                 if check[2*temp]==-1:
-                    check[2*temp]=cnt
+                    check[2*temp]=temp
                     if temp*2==b:
-                        return check[b]
+                        return cnt
                     Q.append(2*temp)
         cnt+=1
-def reversebfs(b,a):
-    ans=[b]
-    Q=deque([b])
-    cnt=check[b]
-    while Q:
-        for _ in range(len(Q)):
-            temp=Q.popleft()
-            if temp==a:
-                return ans
-            if temp%2==0:
-                if check[temp//2]==cnt-1:
-                    Q.append(temp//2)
-                    ans.append(temp//2)
-                    break
-            if temp+1<=100000:
-                if check[temp+1]==cnt-1:
-                    Q.append(temp+1)
-                    ans.append(temp+1)
-                    break
-            if temp-1>=0:
-                if check[temp-1]==cnt-1:
-                    Q.append(temp-1)
-                    ans.append(temp-1)
-                    break
-        cnt-=1
+
 if a==b:
     print(0)
     print(a)
 else:
-    print(bfs(a,b))
-    print(*reversebfs(b,a)[::-1])
+    temp=bfs(a,b)
+    print(temp)
+    ans=[b]
+    while check[b]!=a:
+        ans.append(check[b])
+        b=check[b]
+    ans.append(a)
+    print(*ans[::-1])
