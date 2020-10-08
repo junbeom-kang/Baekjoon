5f9c72d15442cc4aae7b4598fdefd93b0f026091
Kjbjbjb <kgood10369@gmail.com>
2020-10-09 00:01:06 +0900

    no message

--- 데이터마이닝/데이터마이닝6주차.py
+++ 데이터마이닝/데이터마이닝6주차.py
@@ -7,7 +7,7 @@ temp=np.concatenate((iris.data,iris.target.reshape(150,1)),axis=1)
 temp=np.concatenate((temp,np.zeros(shape=(150,1))),axis=1)
 frame=pd.DataFrame(temp,columns=[iris.feature_names+['target','targetName']])
 frame.iloc[:,5]=np.where(frame.iloc[:,4]==0,'setosa',np.where(frame.iloc[:,4]==1,'versicolor','virginica'))
-'''
+
 plt.figure()
 plt.scatter(frame.iloc[:50,0],frame.iloc[:50,1],marker='o',c='red',label='setosa')
 plt.scatter(frame.iloc[50:100,0],frame.iloc[50:100,1],marker='x',c='blue',label='versicolor')
@@ -16,7 +16,6 @@ plt.legend()
 plt.xlabel('sepal length')
 plt.ylabel('sepal width')
 plt.show()
-'''
 plt.figure()
 plt.scatter(frame.iloc[:50,2],frame.iloc[:50,3],marker='o',c='red',label='setosa')
 plt.scatter(frame.iloc[50:100,2],frame.iloc[50:100,3],marker='x',c='blue',label='versicolor')
