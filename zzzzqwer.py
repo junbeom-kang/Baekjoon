82e6ef6726e3c47996a11412ad6b1a596a204b7a
Kjbjbjb <kgood10369@gmail.com>
2020-10-20 15:58:14 +0900

    no message

--- zzzzqwer.py
+++ zzzzqwer.py
@@ -1,13 +1,30 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 import numpy as np
 
-df = pd.DataFrame({'category': ['a', 'a', 'a', 'a',
-'b', 'b', 'b', 'b'],
-'data': np.random.randn(8),
-'weights': np.random.rand(8)})
-df
-grouped = df.groupby('category')
-get_wavg = lambda g: np.average(g['data'], weights=g['weights'])
-grouped.apply(get_wavg)
+df=pd.read_csv('C:/nba.csv',delimiter=',')
+#1번
+temp=df.groupby(['Position','Team'])[['Age','Salary']].mean()
+temp
+#2번
+f=lambda x:x.max()
+gp_by_pos=df.groupby('Position')
+plt.bar(gp_by_pos.apply(f)['Salary'].index,gp_by_pos.apply(f)['Salary'])
 
-np.average([3,1])
+#3번
+plt.figure(figsize=(18,6))
+plt.subplot(2,1,1)
+gp_by_team=df.groupby('Team')
+change=list(gp_by_team['Salary'].apply(f).index)
+for i in range(len(change)):
+    a=change[i].split(' ')
+    b=a[0][0]+a[1][0]
+    change[i]=b
+plt.title('Maxium salary for each tem')
+plt.bar(change,gp_by_team['Salary'].apply(f))
+plt.subplot(2,1,2)
+plt.title('Maxium age for each tem')
+plt.bar(change,gp_by_team['Age'].apply(f))
+
+
+
