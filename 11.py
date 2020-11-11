f03b435d6247044303d98714eac4546da491bcbd
Kjbjbjb <kgood10369@gmail.com>
2020-11-11 20:46:50 +0900

    no message

--- 11.py
+++ 11.py
@@ -1,2 +1,14 @@
-temp=[i for i in range(1,3)]
-print(temp)
+def star(n):
+    if n == 3:
+        return ["***", "* *", "***"]
+    n = n // 3
+    s = star(n)
+    e = [" "*n]*n
+    a = list(map(lambda i: ''.join(i), zip(s, s, s)))
+    b = list(map(lambda i: ''.join(i), zip(s, e, s)))
+    print(a,b)
+    print(a+b)
+    return a + b + a
+print(star(int(input())))
+for s in star(int(input())):
+    print(s)
