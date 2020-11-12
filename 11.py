3a9f8c6723b1d3258a0d749b4b6969c08e8cd1b2
Kjbjbjb <kgood10369@gmail.com>
2020-11-12 23:10:50 +0900

    no message

--- 11.py
+++ 11.py
@@ -1,14 +1,3 @@
-def star(n):
-    if n == 3:
-        return ["***", "* *", "***"]
-    n = n // 3
-    s = star(n)
-    e = [" "*n]*n
-    a = list(map(lambda i: ''.join(i), zip(s, s, s)))
-    b = list(map(lambda i: ''.join(i), zip(s, e, s)))
-    print(a,b)
-    print(a+b)
-    return a + b + a
-print(star(int(input())))
-for s in star(int(input())):
-    print(s)
+a=[(2,3),(4,1)]
+a.sort(key=lambda x:x[1])
+print(a)
