d1722c1c3a490137204fc26cce2394c7c75ed760
Kjbjbjb <kgood10369@gmail.com>
2020-11-21 16:26:58 +0900

    no message

--- 11.py
+++ 11.py
@@ -1,2 +1,15 @@
-temp=[i for i in range(1,3)]
-print(temp)
+n=int(input())
+if n==64:
+    print(1)
+else:
+    ans=0
+    cnt=0
+    temp=64
+    while ans!=n:
+        if ans+(temp>>1)<=n:
+            temp=temp>>1
+            ans+=temp
+            cnt+=1
+        else:
+            temp=temp>>1
+    print(cnt)
