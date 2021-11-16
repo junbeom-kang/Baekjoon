b224ac647f0eb07625c1e5f7e8061a4513742435
junbeom-kang <kgood10369@gmail.com>
2021-11-16 19:41:32 +0900

    no message

--- DFS/1941소문난 칠공주.py
+++ DFS/1941소문난 칠공주.py
@@ -3,43 +3,52 @@ input=sys.stdin.readline
 dx=[0,0,-1,1]
 dy=[1,-1,0,0]
 def solution():
-    global cnt,q,w
-    def dfs(x,y):
-        global cnt,q,w
+    global cnt
+    def check(l):
+        global temp,cnt
+        def dfs(x,y):
+            global temp
+            temp+=1
+            for t in range(4):
+                nx=x+dx[t]
+                ny=y+dy[t]
+                if 0<=nx<5 and 0<=ny<5 and [nx,ny] in l and not visited[nx][ny]:
+                    visited[nx][ny]=True
+                    dfs(nx,ny)
+
+        temp=0
+        visited=[[False]*5 for _ in range(5)]
+        visited[l[0][0]][l[0][1]]=True
+        dfs(l[0][0],l[0][1])
+        if temp==7:
+            cnt+=1
+
+    def go(start,l,q,w):
         if w==4:
             return
         if q+w==7:
-            print(x,y,q,w,"!!!!")
-            cnt+=1
+            candidate.append(l)
             return
-        for t in range(4):
-            nx=x+dx[t]
-            ny=y+dy[t]
-            if 0<=nx<5 and 0<=ny<5:
-                if not visited[nx][ny] and not temp[nx][ny]:
-                    temp[nx][ny] = True
-                    if arr[nx][ny]=='Y':
-                        dfs(nx,ny)
-                    else:
-                        dfs(nx,ny)
-                    temp[nx][ny]=False
-    cnt=0
+        for i in range(start+1,25):
+            if arr[index[i][0]][index[i][1]]=='Y':
+                go(i,l+[[index[i][0],index[i][1]]],q,w+1)
+            else:
+                go(i,l+[[index[i][0],index[i][1]]],q+1,w)
+    candidate=[]
     arr=[]
+    index=[]
     for _ in range(5):
         arr.append(list(input().rstrip()))
-    visited=[[False]*5 for _ in range(5)]
-    for i in range(5):
-        for j in range(5):
-            temp = [[False] * 5 for _ in range(5)]
-            temp[i][j]=True
-            q,w=0,0
-            if arr[i][j]=='Y':
-                w=1
-                dfs(i,j)
-            else:
-                q=1
-                dfs(i,j)
-            visited[i][j]=True
+        for __ in range(5):
+            index.append([_,__])
+    for i in range(19):
+        if arr[index[i][0]][index[i][1]]=='Y':
+            go(i,[[index[i][0],index[i][1]]],0,1)
+        else:
+            go(i,[[index[i][0],index[i][1]]],1,0)
+    cnt=0
+    for i in candidate:
+        check(i)
     print(cnt)
     return
 
