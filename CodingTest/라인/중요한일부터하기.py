fec33ec1bd303e71914747e10548d531284a8a65
junbeom-kang <kgood10369@gmail.com>
2021-11-25 18:26:13 +0900

    no message

--- CodingTest/라인/중요한일부터하기.py
+++ CodingTest/라인/중요한일부터하기.py
@@ -1,23 +1,50 @@
+import sys
+from collections import deque,defaultdict
+input=sys.stdin.readline
 
-from collections import defaultdict
 def solution(jobs):
-    dic=defaultdict(int)
-    l=len(jobs)
-    index=1
-    start,end,num,weight=jobs[0]
-    now=num
-    while jobs[index][0]<=start+end:
-        if jobs[index][2]==num:
-            end+=jobs[index][1]
-        else:
-            dic[jobs[index][2]]+=jobs[index][3]
-    sorted(dic.items(),reverse=True,key=lambda item:item[1])
-    sorted(dic.keys(),reverse=True)
+    temp=deque(jobs)
+    answer=[]
+    n=len(jobs)
+    work=defaultdict(int)
+    weight=defaultdict(int)
+    last=temp[-1][0]
+    start,time,now,w=temp.popleft()
+    answer.append(now)
+    end=time+start
+    for i in range(last+1):
+        if end<i:
+            end=i
+        #print(end)
+        if temp[0][0]==i:
+            start,time,num,w=temp.popleft()
+            work[num]+=time
+            if weight[num]:
+                weight[num]=w
+            else:
+                if weight[num]<w:
+                    weight[num]=w
+        if i==end and weight:
+            if weight[now]:
+                del weight[now]
+                end+=work[now]
+                work[now]=0
+            else:
+                del weight[now]
+                next=sorted(weight.items(),reverse=True,key=lambda x:(x[1],-x[0]))[0][0]
+                answer.append(next)
+                end+=work[next]
+                work[next]=0
+                del weight[next]
+                now=next
+    if weight:
+        for i in sorted(weight.items(),reverse=True,key=lambda x:(x[1],-x[0])):
+            answer.append(i[0])
+    print(answer)
+    return
 
-
-
-
-    return answer
 
 if __name__ == '__main__':
     solution([[1, 5, 2, 3], [2, 2, 3, 2], [3, 1, 3, 3], [5, 2, 1, 5], [7, 1, 1, 1], [9, 1, 1, 1], [10, 2, 2, 9]])
+    solution([[1,2,1,5], [2,1,2,100], [3,2,1,5], [5, 2, 1, 5]])
+    solution([[0,2,3,1], [5,3,3,1], [10,2,4,1]])
--- test.py
+++ test.py
@@ -1,41 +1,5 @@
-import sys
-input=sys.stdin.readline
-
-def solution():
-    def get(z):
-        if z == 'B':
-            s1, s2 = 0, score['B']
-        elif z == 'S':
-            s1, s2 = score['B'], score['S']
-        elif z == 'G':
-            s1, s2 = score['S'], score['G']
-        elif z == 'P':
-            s1, s2 = score['G'], score['P']
-        else:
-            s1, s2 = score['P'], score['D']
-        return s1,s2
-    n=int(input())
-    s,g,p,d=map(int,input().split())
-    r = input().rstrip()
-    score={'B':s,'S':g,'G':p,'P':d,'D':d+1}
-    dp=[[0]*501 for _ in range(n)]
-    s1,s2=get(r[0])
-    for i in range(s1,s2):
-        dp[0][i]=i
-    for i in range(1,n):
-        for j in range(score[r[i]]):
-            s1,s2=get(r[i])
-            if r[i]=='D':
-                for t in range(s1 - j, s2):
-                    if dp[i - 1][t]:
-                        dp[i][j] = max(dp[i][j], dp[i - 1][t] + j)
-            else:
-                for t in range(s1-j,s2-j):
-                    if t>=0:
-                        dp[i][j]=max(dp[i][j],dp[i-1][t]+j)
-    print(max(dp[n-1]))
-    return
-
-if __name__ == '__main__':
-    solution()
-
+from collections import defaultdict
+a=defaultdict(int)
+if not a[1]:
+    print("T")
+print(a)
